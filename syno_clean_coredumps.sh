f3269f95f93640d0809a7b388eaaf4c02d87bc5b
Dave Russell <39733752+007revad@users.noreply.github.com>
2025-11-08 10:11:50 +1100

    Update script version and improve core dump deletion
    v1.2.4
    - Changed to not use bc for flaoting point math as DSM does not included bc.
    - Changed to check if running as sudo or root.
    - Refactored code.
    - Bug fix for deleting any file with `core` in its name even if it was in a sub folder. Issue #19
    - Bug fix for incorrectly searching sub folders. Issue #19

--- syno_clean_coredumps.sh
+++ syno_clean_coredumps.sh
@@ -6,18 +6,30 @@
 #   -a #, --age #   where # is number of days
 #------------------------------------------------------------
 
-scriptver="v1.1.3"
+scriptver="v1.2.4"
 script=Synology_Cleanup_Coredumps
 repo="007revad/Synology_Cleanup_Coredumps"
 scriptname=syno_cleanup_coredumps
 
 echo -e "Synology_cleanup_coredumps $scriptver by github.com/007revad\n"
 
+ding(){ 
+    printf \\a
+}
+
 # Check script is running on a Synology NAS
 if ! /usr/bin/uname -a | grep -i synology >/dev/null; then
+    ding
     echo "This script is NOT running on a Synology NAS!"
     echo "Copy the script to a folder on the Synology"
     echo "and run it from there."
+    exit 1
+fi
+
+# Check script is running as root
+if [[ $( whoami ) != "root" ]]; then
+    ding
+    echo -e "${Error}ERROR${Off} This script must be run as sudo or root!"
     exit 1
 fi
 
@@ -46,48 +58,32 @@ for volume in /volume*; do
             if [[ $age =~ [0-9]+ ]]; then
                 # Delete core dumps older than $age
                 echo -e "Deleting core dumps older than $age days on ${volume}"
-                #find "$volume"/@*.core* -mtime +"$age" -delete
-
-                # Find and delete old .core files, then report how many and total size deleted
-                find "$volume"/@*.core* -mtime +"$age" -type f -printf '%s\n' 2>/dev/null \
-                | awk '{count++; sum+=$1} END {printf "%d %.0f\n", count, sum}' \
-                | {
-                    read -r count sum
-                    if [[ $count -gt 0 ]]; then
-                        # Delete the files
-                        find "$volume"/@*.core* -mtime +"$age" -type f -delete
-
-                        # Print summary
-                        total_mb=$(echo "$sum / 1024 / 1024" | bc -l)
-                        #printf "Deleted %d files (total %.2f MB) on %s\n\n" "$count" "$total_mb" "$volume"
-                        printf "Deleted %d files (total %.2f MB)\n\n" "$count" "$total_mb"
-                    else
-                        echo -e "No files to delete.\n"
-                    fi
-                }
             else
                 # Delete all core dumps
                 echo -e "Deleting all core dumps on ${volume}"
-                #find "$volume"/@*.core* -delete
+            fi
 
-                # Find and delete all .core files, then report how many and total size deleted
-                find "$volume"/@*.core* -type f -printf '%s\n' 2>/dev/null \
-                | awk '{count++; sum+=$1} END {printf "%d %.0f\n", count, sum}' \
-                | {
-                    read -r count sum
-                    if [[ $count -gt 0 ]]; then
-                        # Delete the files
-                        find "$volume"/@*.core* -type f -delete
+            # Find and delete old .core files, then report how many and total size deleted
+            #find "$volume" -maxdepth 1 -mtime +"$age" \( -name "@*.core" -o -name "@*.core.gz" \) -type f -printf '%s\n' 2>/dev/null \
+            find "$volume" -maxdepth 1 -mmin +$((60*24*age)) \( -name "@*.core" -o -name "@*.core.gz" \) -type f -printf '%s\n' 2>/dev/null \
+            | awk '{count++; sum+=$1} END {printf "%d %.0f\n", count, sum}' \
+            | {
+                read -r count sum
+                if [[ $count -gt 0 ]]; then
+                    # Delete the files
+                    if find "$volume" -maxdepth 1 -mmin +$((60*24*age)) \( -name "@*.core" -o -name "@*.core.gz" \) -type f -delete; then
 
                         # Print summary
-                        total_mb=$(echo "$sum / 1024 / 1024" | bc -l)
+                        total_mb=$(echo "$sum" | awk '{ megabytes = $1 / 1024 / 1024; printf "%.2f", megabytes }')
                         #printf "Deleted %d files (total %.2f MB) on %s\n\n" "$count" "$total_mb" "$volume"
                         printf "Deleted %d files (total %.2f MB)\n\n" "$count" "$total_mb"
                     else
-                        echo -e "No files to delete.\n"
+                        echo ""
                     fi
-                }
-            fi
+                else
+                    echo -e "No files to delete.\n"
+                fi
+            }
         fi
         #echo ""
 
